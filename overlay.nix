af87bb8c30ce1290d75bdeb348cf25a64ab31632
bostan <andrebostan@gmail.com>
2025-12-28 10:08:08 +0300

    Avoid duplicate Nautilus extension paths

--- overlay.nix
+++ overlay.nix
@@ -10,8 +10,8 @@ final: prev: {
 
     postFixup = (old.postFixup or "") + ''
       wrapProgram "$out/bin/nautilus" \
-        --run 'if [ -z "''${XDG_DATA_DIRS:-}" ]; then export XDG_DATA_DIRS=/run/current-system/sw/share:/etc/xdg; else export XDG_DATA_DIRS="''${XDG_DATA_DIRS}:/run/current-system/sw/share:/etc/xdg"; fi' \
-        --run 'if [ -z "''${NAUTILUS_EXTENSION_DIRS:-}" ]; then export NAUTILUS_EXTENSION_DIRS=/run/current-system/sw/lib/nautilus/extensions-4; else export NAUTILUS_EXTENSION_DIRS="''${NAUTILUS_EXTENSION_DIRS}:/run/current-system/sw/lib/nautilus/extensions-4"; fi'
+        --run 'for d in /run/current-system/sw/share /etc/xdg; do case ":''${XDG_DATA_DIRS:-}:" in *:"$d":*) ;; "") XDG_DATA_DIRS="$d" ;; *) XDG_DATA_DIRS="''${XDG_DATA_DIRS}:$d" ;; esac; done; export XDG_DATA_DIRS' \
+        --run 'for d in /run/current-system/sw/lib/nautilus/extensions-4; do case ":''${NAUTILUS_EXTENSION_DIRS:-}:" in *:"$d":*) ;; "") NAUTILUS_EXTENSION_DIRS="$d" ;; *) NAUTILUS_EXTENSION_DIRS="''${NAUTILUS_EXTENSION_DIRS}:$d" ;; esac; done; export NAUTILUS_EXTENSION_DIRS'
     '';
   });
 }
